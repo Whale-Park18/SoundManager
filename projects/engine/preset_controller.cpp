484166b587b3e5b1261792eb89dcc9869c5402c8
Park18 <whale.park18@gmail.com>
2025-06-15 20:33:44 +0900

    feat (#1): 프리셋 컨트롤러 정의

---
+++ projects/engine/preset_controller.cpp
@@ -0,0 +1,1 @@
+#include "preset_controller.h"
---
+++ projects/engine/src/json/preset_controller.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <rapidjson/document.h>
+
+class PresetController
+{
+public:
+
+	// json 초기화
+
+
+	// 읽기
+
+	// 쓰기
+
+private:
+
+};
+
